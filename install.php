000fa709714633ab607e0f46c44bfeec80881b5b
Laurent <laurent.paganin@gmail.com>
2015-09-30 18:38:49 +0200

    update install

--- install.php
+++ install.php
@@ -1,20 +1,35 @@
 <?php
-
 // include class.secure.php to protect this file and the whole CMS!
-if (defined('WB_PATH')) {	
-	include(WB_PATH.'/framework/class.secure.php'); 
+if (defined('WB_PATH')) {
+	include(WB_PATH.'/framework/class.secure.php');
 } else {
-	$root = "../";
+	$oneback = "../";
+	$root = $oneback;
 	$level = 1;
 	while (($level < 10) && (!file_exists($root.'/framework/class.secure.php'))) {
-		$root .= "../";
+		$root .= $oneback;
 		$level += 1;
 	}
-	if (file_exists($root.'/framework/class.secure.php')) { 
-		include($root.'/framework/class.secure.php'); 
+	if (file_exists($root.'/framework/class.secure.php')) {
+		include($root.'/framework/class.secure.php');
 	} else {
 		trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
 	}
 }
 // end include class.secure.php
 
+/** @var $entityManager \Doctrine\ORM\EntityManager */
+global $entityManager;
+if (!isset($entityManager)) {
+	require_once(LEPTON_PATH."/modules/lib_doctrine/library.php");
+}
+
+$tool = new \Doctrine\ORM\Tools\SchemaTool($entityManager);
+
+$classes = array(
+	$entityManager->getClassMetadata('\Group'),
+	$entityManager->getClassMetadata('\Page'),
+	$entityManager->getClassMetadata('\Section'),
+	$entityManager->getClassMetadata('\User'),
+);
+$tool->updateSchema($classes, true);
